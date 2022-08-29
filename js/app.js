1b66dea4501dee238f763bbcd64274e61338b5d4
Herman Chen <hermanchen77@gmail.com>
2022-08-29 19:37:00 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let siteVisitor = prompt('What is your name?');
+
+alert(`Welcome to my site ${siteVisitor}!`);
+
+let color = prompt('what is your favorite color?');
+
+switch 
+console.log('app is running');
